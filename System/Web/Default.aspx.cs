001f0233c2f6fe4e3857f6dd328c9b1e61bd413b
Alex Denysenko <self@alexdenysenko.com>
2013-06-25 19:25:43 -0400

    Added solution, web and web.tests projects, Simple hello world

---
+++ System/Web/Default.aspx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Web
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { }
+    }
+}
