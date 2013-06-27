084687577dd718eadf82ed81d8dc272c2da4aca3
Alex Denysenko <self@alexdenysenko.com>
2013-06-27 07:15:41 -0400

    Solved issue with border between divs

--- System/Web/Global.asax.cs
+++ System/Web/Global.asax.cs
@@ -1,12 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Web
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
