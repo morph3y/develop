136fc3e86b88a71db727c856fa85a2df9701aa13
Alex Denysenko <self@alexdenysenko.com>
2013-07-05 23:09:10 -0400

    Revert: Authentication feature

--- System/Business/BusinessDependencyContainer.cs
+++ System/Business/BusinessDependencyContainer.cs
@@ -1,7 +1,6 @@
 using Business.Contracts;
 using Business.Services.Objects;
 using Business.Services.UserAccess;
-using Business.UserAccess;
 using Ninject.Modules;
 
 namespace Business
@@ -11,7 +10,6 @@ namespace Business
         public override void Load()
         {
             Bind<IObjectService>().To<ObjectService>().InTransientScope();
-            Bind<IAuthentication>().To<Authentication>().InTransientScope();
             Bind<IUserAccessManager>().To<UserAccessManager>().InThreadScope();
         }
     }
